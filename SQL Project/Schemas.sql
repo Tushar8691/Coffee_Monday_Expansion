5751d524987ab6a54a1494468243cb402bd8e231
Tushar <165695733+Tushar8691@users.noreply.github.com>
2025-12-24 00:08:43 +0530

    update
    Removed existing DROP TABLE statements for sales, customers, products, and city.

--- SQL Project/Schemas.sql
+++ SQL Project/Schemas.sql
@@ -1,16 +1,4 @@
--- Monday Coffee SCHEMAS
-
-DROP TABLE IF EXISTS sales;
-DROP TABLE IF EXISTS customers;
-DROP TABLE IF EXISTS products;
-DROP TABLE IF EXISTS city;
-
--- Import Rules
--- 1st import to city
--- 2nd import to products
--- 3rd import to customers
--- 4th import to sales
-
+-- Coffee_Monday SCHEMAS
 
 CREATE TABLE city
 (
